f075b4210059d5c3f5689e88acbe9aa2e4b19d5f
nirnanaaa <mosny@zyg.li>
2014-08-22 10:00:07 +0200

    updated init

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_mail_template do
   name 'Redmine Mail Template plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Florian Kasper'
+  description 'Gives the redmine email templates a bit more flair'
+  version '0.0.2'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
