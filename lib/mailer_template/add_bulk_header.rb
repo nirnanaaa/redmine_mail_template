f246b74153a65ca276db08bb933e37c51e99470c
nirnanaaa <mosny@zyg.li>
2014-08-22 10:30:12 +0200

    changed headers

--- lib/mailer_template/add_bulk_header.rb
+++ lib/mailer_template/add_bulk_header.rb
@@ -1,7 +1,8 @@
 module MailerTemplate
   class AddBulkHeader < ::Rails::Railtie
     config.after_initialize do
-      config.action_mailer.default "Presendence" => 'Bulk'
+      ActionMailer::Base.default "Presendence" => 'Bulk'
+      ActionMailer::Base.default "X-Presendence" => 'Chunk'
     end
   end
 end
