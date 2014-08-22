ad9487b841ad992c10e34a00ebe8441f3a54825f
nirnanaaa <mosny@zyg.li>
2014-08-22 10:38:52 +0200

    changed charset

--- lib/mailer_template/add_bulk_header.rb
+++ lib/mailer_template/add_bulk_header.rb
@@ -3,6 +3,7 @@ module MailerTemplate
     config.after_initialize do
       ActionMailer::Base.default "Precendence" => 'Bulk'
       ActionMailer::Base.default "X-Precendence" => 'Chunk'
+      ActionMailer::Base.charset 'utf-8'
     end
   end
 end
