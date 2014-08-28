0e7c747709e2e1006fd4826ceb6ddfc9fb9e5cb9
nirnanaaa <mosny@zyg.li>
2014-08-28 14:11:02 +0200

    updated bulk headers

--- lib/mailer_template/add_bulk_header.rb
+++ lib/mailer_template/add_bulk_header.rb
@@ -1,8 +1,7 @@
 module MailerTemplate
   class AddBulkHeader < ::Rails::Railtie
     config.after_initialize do
-      ActionMailer::Base.default "Precendence" => 'Bulk'
-      ActionMailer::Base.default "X-Precendence" => 'Chunk'
+      ActionMailer::Base.default "Precendence" => 'bulk'
     end
   end
 end
