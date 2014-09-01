939287f04b9dccf0328163f3f53da02d2924a4d4
nirnanaaa <mosny@zyg.li>
2014-09-01 08:44:53 +0200

    updated mail headers again...

--- lib/mailer_template/add_bulk_header.rb
+++ lib/mailer_template/add_bulk_header.rb
@@ -1,7 +1,7 @@
 module MailerTemplate
   class AddBulkHeader < ::Rails::Railtie
     config.after_initialize do
-      ActionMailer::Base.default "Precendence" => 'bulk'
+      ActionMailer::Base.default "Precedence" => 'bulk'
     end
   end
 end
